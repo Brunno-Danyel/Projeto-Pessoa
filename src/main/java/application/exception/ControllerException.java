e0af2278a1445329d0cf50821f985db89105a169
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-01-16 22:16:58 -0300

    Implementação das classes de exception's

---
+++ src/main/java/application/exception/ControllerException.java
@@ -0,0 +1,40 @@
+package application.exception;
+
+import application.ApiErrors;
+import application.entities.Endereco;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ControllerException {
+
+    @ExceptionHandler(PessoaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handlePessoaException(PessoaException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+    @ExceptionHandler(EnderecoException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleEnderecoException(EnderecoException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
+    }
+}
---
+++ src/main/java/application/exception/PessoaException.java
@@ -0,0 +1,8 @@
+package application.exception;
+
+public class PessoaException extends RuntimeException {
+    public PessoaException(String msg){
+        super(msg);
+    }
+
+}
