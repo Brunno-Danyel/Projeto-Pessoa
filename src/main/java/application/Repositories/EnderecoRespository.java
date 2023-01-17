c069fcd3b0cb83f1404a71307f6fcf39a555cfc2
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2023-01-17 19:05:17 -0300

    Implementação do método JPA para a chamada dos endereços

--- src/main/java/application/Repositories/EnderecoRespository.java
+++ src/main/java/application/Repositories/EnderecoRespository.java
@@ -1,9 +1,14 @@
 package application.Repositories;
 
 import application.entities.Endereco;
+import application.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public interface EnderecoRespository extends JpaRepository<Endereco, Long> {
 
+        Optional<List<Endereco>> findByPessoa(Pessoa pessoa);
 }
